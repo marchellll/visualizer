63def29b8df60105795aceb160fa862c33eebd6f
Marchell Imanuel <imanuel.marchell@gmail.com>
2021-08-17 00:28:44 +0700

    fix quick sort bug

--- src/algorithms/sorting/quickSort.js
+++ src/algorithms/sorting/quickSort.js
@@ -70,7 +70,7 @@ const inplaceQuickSort = async (arr, from, to, callbacks) => {
     return;
   }
 
-  if (to - from <= 1) {
+  if (to - from < 1) {
     // empty or single member array
     return;
   }
---
+++ tests/unit/algorithms/bubbleSort.spec.js
@@ -0,0 +1,57 @@
+import _ from 'lodash';
+import bubbleSort from '../../../src/algorithms/sorting/bubbleSort';
+
+const noopCallbacks = {
+  isRunning: () => true,
+  onComparing: () => { },
+  onSwap: () => { },
+  afterSwap: () => { },
+  onPivotChoosen: () => { },
+  onPivotDismiss: () => { },
+  log: () => { },
+};
+
+test('sorted', async () => {
+  const arr = [1, 2, 3, 4, 5, 6];
+  const input = [...arr];
+
+  await bubbleSort(input, noopCallbacks);
+
+  expect(input).toEqual(arr);
+});
+
+test('reverse sorted', async () => {
+  const arr = [1, 2, 3, 4, 5, 6];
+  const input = _.reverse([...arr]);
+
+  await bubbleSort(input, noopCallbacks);
+
+  expect(input).toEqual(arr);
+});
+
+test('nearly sorted', async () => {
+  const arr = [1, 2, 3, 5, 4, 6];
+
+  await bubbleSort(arr, noopCallbacks);
+
+  expect(arr).toEqual(arr.sort());
+});
+
+test('not unique', async () => {
+  const arr = [1, 1, 1, 2, 2, 4, 4, 4];
+
+  await bubbleSort(arr, noopCallbacks);
+
+  expect(arr).toEqual(arr.sort());
+});
+
+Array.from({ length: 500 }).forEach(() => {
+  test('flaky random', async () => {
+    const arr = Array.from({ length: 50 }).map(() => parseInt(Math.floor(Math.random() * 100), 10));
+    const input = [...arr];
+
+    await bubbleSort(input, noopCallbacks);
+
+    expect(input).toEqual(arr.sort((a, b) => a - b));
+  });
+});
